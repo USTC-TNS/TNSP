78237f1dfd0ddccefa46e98854a24d7709e51ed3
Hao Zhang <zh970205@outlook.com>
2019-03-23 16:45:10 +0800

    add-license-to-top-of-program

--- TAT.cc
+++ TAT.cc
@@ -1,3 +1,76 @@
+/* TAT
+ * Copyright (C) 2019  Hao Zhang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* TODO LIST
+ * - Truncated SVD
+ * - QR
+ * - all kind of Norm
+ * - use it
+ */
+
+/* back for old cuda code
+namespace shuffle
+{
+  void shuffle(PlainData    data_new,
+                PlainData    data_old,
+                const Dims&  dims_new,
+                const Dims&  dims_old,
+                const Order& plan)
+  {
+    //Stream !!!
+    const Rank& size = plan.size();
+    std::vector<int> int_plan(size, 0);//(plan.begin(), plan.end());
+    std::vector<int> int_dims(size, 0);//(dims_old.begin(), dims_old.end());
+    for(Rank i=0;i<size;i++)
+      {
+        int_plan[i] = size - plan[size-i-1] -1;
+        int_dims[i] = dims_old[size-i-1];
+        //std::cout << plan[i] << "\t" << int_plan[i] << "\t" << dims_old[i] << "\t" << int_dims[i] << "\n";
+      }
+    //std::cout << "\n\n\n";
+    cuttHandle handle;
+    internal::cuda::Stream* stream = internal::cuda::get_stream();
+    cuttPlan(&handle, size, int_dims.data(), int_plan.data(), sizeof(Base), stream->stream);
+    cuttExecute(handle, data_old, data_new);
+    cudaStreamSynchronize(stream->stream);
+    internal::cuda::delete_stream(stream);
+  }
+}
+
+namespace contract
+{
+  template<>
+  void gemm<double>(double* data,
+                    double* data1,
+                    double* data2,
+                    Size    a,
+                    Size    b,
+                    Size    c)
+  {
+    double alpha = 1;
+    double beta  = 0;
+    internal::cuda::Stream* stream = internal::cuda::get_stream();
+    cublasDgemm(stream->handle, CUBLAS_OP_N, CUBLAS_OP_N, c, a, b, &alpha, data2, c, data1, b, &beta, data, c);
+    cudaStreamSynchronize(stream->stream);
+    internal::cuda::delete_stream(stream);
+  }
+}
+ */
+
 #ifndef TAT_HPP_
 
 #include <cassert>
