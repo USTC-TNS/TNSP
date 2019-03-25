3b67deba9f1bdb4a73e18d0ed1fe2da0e0e64781
Hao Zhang <hao.zhang.xyz@gmail.com>
2019-03-26 03:55:04 +0800

    改一下todolist的位置

--- README.md
+++ README.md
@@ -18,3 +18,62 @@ now, I am writing this tensor library finally.
 ## dependence
 - [args](https://github.com/Taywee/args) MIT License
 - [hptt](https://github.com/springer13/hptt) BSD 3-Clause License
+
+---
+
+## TODO LIST
+- transpose may be optimized
+- Truncated SVD
+- dgegqr magma
+- cuda，sw
+- use it, peps, kitaev, hubbard
+
+### note for some of cuda program
+```c++
+namespace shuffle
+{
+  void shuffle(PlainData    data_new,
+               PlainData    data_old,
+               const Dims&  dims_new,
+               const Dims&  dims_old,
+               const Order& plan)
+  {
+    //Stream !!!
+    const Rank& size = plan.size();
+    std::vector<int> int_plan(size, 0);//(plan.begin(), plan.end());
+    std::vector<int> int_dims(size, 0);//(dims_old.begin(), dims_old.end());
+    for(Rank i=0;i<size;i++)
+      {
+        int_plan[i] = size - plan[size-i-1] -1;
+        int_dims[i] = dims_old[size-i-1];
+        //std::cout << plan[i] << "\t" << int_plan[i] << "\t" << dims_old[i] << "\t" << int_dims[i] << "\n";
+      }
+    //std::cout << "\n\n\n";
+    cuttHandle handle;
+    internal::cuda::Stream* stream = internal::cuda::get_stream();
+    cuttPlan(&handle, size, int_dims.data(), int_plan.data(), sizeof(Base), stream->stream);
+    cuttExecute(handle, data_old, data_new);
+    cudaStreamSynchronize(stream->stream);
+    internal::cuda::delete_stream(stream);
+  }
+}
+
+namespace contract
+{
+  template<>
+  void gemm<double>(double* data,
+                    double* data1,
+                    double* data2,
+                    Size    a,
+                    Size    b,
+                    Size    c)
+  {
+    double alpha = 1;
+    double beta  = 0;
+    internal::cuda::Stream* stream = internal::cuda::get_stream();
+    cublasDgemm(stream->handle, CUBLAS_OP_N, CUBLAS_OP_N, c, a, b, &alpha, data2, c, data1, b, &beta, data, c);
+    cudaStreamSynchronize(stream->stream);
+    internal::cuda::delete_stream(stream);
+  }
+}
+```
--- test.cpp
+++ test.cpp
@@ -16,17 +16,295 @@
  */
 
 #define TAT_USE_CPU
-
-// SVD
-// #define TAT_USE_GESDD
-// #define TAT_USE_GESVD
 #define TAT_USE_GESVDX
-
-// QR
 #define TAT_USE_GEQRF
-// #define TAT_USE_GEQP3
-// GEQP3 not understand, maybe useful if R will drop
-
-#define TAT_TEST_MAIN
 
 #include "TAT.hpp"
+
+using namespace TAT;
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cout << "scalar\n";
+  {
+    // scalar
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_zero();
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      t1 += 1.2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      t1 -= 1.2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      t1 *= 1.2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      t1 /= 1.2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      t1 += t2;
+      std::cout << t1*2.3 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_zero();
+      t2.set_test();
+      t1 -= t2;
+      std::cout << 1-t1/3.4 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      std::cout << 1+3/(t1+1)+t2 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      std::cout << +(t1-1.2)-t2 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      std::cout << 3+1.2/(t1*1.2) << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      t1.set_test();
+      std::cout << -(2.4*(t1/1.2)) << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      std::cout << t1/t2 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      t2+=1;
+      t1/=t2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      std::cout << t1* t2 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3}, {Up, Down});
+      Tensor<> t2({2, 3}, {Up, Down});
+      t1.set_test();
+      t2.set_test();
+      t1 += 1;
+      t1 *= t2;
+      std::cout << t1 << std::endl;
+    }
+    {
+      //Tensor<> t1({2},{});
+    }
+    {
+      //Tensor<> t1({2,3},{Down,Down});
+    }
+  } // scalar
+  std::cout << "transpose\n";
+  {
+    // transpose
+    {
+      Tensor<> t1({2, 3}, {Left, Right});
+      t1.set_test();
+      auto t2 = t1.transpose({Right, Left});
+      std::cout << t1 << std::endl << t2 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3, 4, 5}, {Down, Up, Left, Right});
+      t1.set_test();
+      auto t2 = t1.transpose({Left, Down, Right, Up});
+      std::cout << t1 << std::endl << t2 << std::endl;
+    }
+    {
+      //Tensor<> t1({2,3},{Left,Right});
+      //auto t2 = t1.transpose({Right,Down});
+    }
+    {
+      //Tensor<> t1({2,3},{Left,Right});
+      //auto t2 = t1.transpose({Right,Left,Left});
+    }
+    {
+      //Tensor<> t1({2,3},{Left,Right});
+      //auto t2 = t1.transpose({Right,Right});
+    }
+  } // transpose
+  std::cout << "to\n";
+  {
+    // to
+    {
+      Tensor<> t1({2, 3}, {Left, Right});
+      t1.set_test();
+      Tensor<Device::CPU, int> t2 = t1.to<int>();
+      std::cout << t1 << std::endl << t2 << std::endl;
+    }
+  } // to
+  std::cout << "contract\n";
+  {
+    // contract
+    {
+      Tensor<> t1({2, 3}, {Down, Up});
+      Tensor<> t2({2, 3}, {Down, Up});
+      t1.set_test();
+      t2.set_test();
+      std::cout << t1 << std::endl << t2 << std::endl << Tensor<>::contract(t1, t2, {Up}, {Up}, {}, {{Down, Down1}}) << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3, 4, 5, 6}, {Down, Up, Left, Right, Phy});
+      Tensor<> t2({5, 3, 7}, {Down, Up, Left});
+      t1.set_test();
+      t2.set_test();
+      std::cout << t1 << std::endl << t2 << std::endl << Tensor<>::contract(t1, t2, {Up, Right}, {Up, Down}, {}, {{Left, Left3}}) << std::endl;
+    }
+    {
+      //Tensor<> t1({2,3}, {Down, Up});
+      //Tensor<> t2({2,3}, {Down, Up});
+      //Tensor<>::contract(t1, t2, {Up}, {Left}, {}, {{Down, Down1}});
+    }
+    {
+      //Tensor<> t1({2,3}, {Down, Up});
+      //Tensor<> t2({2,3}, {Down, Up});
+      //Tensor<>::contract(t1, t2, {Up}, {Down}, {}, {{Up, Down1}});
+    }
+    {
+      //Tensor<> t1({2,3}, {Down, Up});
+      //Tensor<> t2({2,3}, {Down, Up});
+      //Tensor<>::contract(t1, t2, {Up,Down}, {Up, Up}, {}, {{Up, Down1}});
+    }
+  } // contract
+  std::cout << "multiple\n";
+  {
+    // multiple
+    {
+      Tensor<> t1({3, 4}, {Down, Up});
+      Tensor<> t2({4}, {Down});
+      t1.set_test();
+      t2.set_test();
+      auto t3 = t1.multiple(t2, Up);
+      std::cout << t1 << std::endl << t2 << std::endl << t3 << std::endl;
+    }
+    {
+      Tensor<> t1({2, 3, 4}, {Right, Down, Up});
+      Tensor<> t2({3}, {Down});
+      t1.set_test();
+      t2.set_test();
+      auto t3 = t1.multiple(t2, Down);
+      std::cout << t1 << std::endl << t2 << std::endl << t3 << std::endl;
+    }
+    {
+      //Tensor<> t1({2,3,4}, {Right,Down, Up});
+      //Tensor<> t2({3}, {Down});
+      //t1.set_test();
+      //t2.set_test();
+      //auto t3 = t1.multiple(t2, Up);
+      //std::cout << t1 << std::endl << t2 << std::endl << t3 << std::endl;
+    }
+  } // multiple
+  std::cout << "svd\n";
+  {
+    // svd
+    {
+      Tensor<> t1({4, 6}, {Left, Right});
+      t1.set_test();
+      auto res = t1.svd({Left}, Right, Down, 4);
+      std::cout << res.U << std::endl << res.S << std::endl << res.V << std::endl;
+    }
+    {
+      Tensor<> t1({2, 2, 3, 2}, {Left, Right, Up, Down});
+      t1.set_test();
+      auto res = t1.svd({Left, Right}, Right1, Down1);
+      std::cout << res.U << std::endl << res.S << std::endl << res.V << std::endl;
+    }
+    {
+      Tensor<> t1({2, 2, 3, 2}, {Left, Right, Up, Down});
+      t1.set_test();
+      auto res = t1.svd({Left, Down}, Right1, Down1, -1);
+      std::cout << res.U << std::endl << res.S << std::endl << res.V << std::endl;
+    }
+    {
+      Tensor<> t1({2, 2, 3, 2}, {Left, Right, Up, Down});
+      t1.set_test();
+      auto res = t1.svd({Left, Down}, Right1, Down1, 3);
+      std::cout << res.U << std::endl << res.S << std::endl << res.V << std::endl;
+      std::ofstream f2;
+      f2.open("test_io2.out");
+      f2 << res.V;
+      f2.close();
+    }
+  } // svd
+  std::cout << "io\n";
+  {
+    // io
+    {
+      Tensor<> t1({2, 2, 3, 2}, {Left, Right, Up, Down});
+      t1.set_test();
+      std::cout << t1 << std::endl;
+      std::ofstream f1;
+      f1.open("test_io.out");
+      f1 << t1;
+      f1.close();
+      Tensor<> t2;
+      std::ifstream f2;
+      f2.open("test_io.out");
+      f2 >> t2;
+      f2.close();
+      std::cout << t2 << std::endl;
+    }
+  } // io
+  std::cout << "qr\n";
+  {
+    // qr
+    {
+      Tensor<> t1({4, 6}, {Left, Right});
+      t1.set_test();
+      auto res = t1.qr({Left}, Right, Down);
+      std::cout << res.Q << std::endl << res.R << std::endl;
+    }
+    {
+      Tensor<> t1({4, 6}, {Left, Right});
+      t1.set_test();
+      auto res = t1.qr({Right}, Up, Down);
+      std::cout << res.Q << std::endl << res.R << std::endl;
+    }
+  } // qr
+  return 0;
+} // main
