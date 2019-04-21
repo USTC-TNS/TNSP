a9cfecfbe82880af0849f291a71455098f09f2e4
Hao Zhang <hao.zhang.xyz@gmail.com>
2019-04-21 11:47:14 +0800

    structure beautify

--- test_with_yq.cpp
+++ test_with_yq.cpp
@@ -43,7 +43,22 @@ std::ostream& operator<<(std::ostream& os, const std::vector<Tensor>& value) {
   return os;
 }
 
+TAT::Legs get_leg(int i) {
+  return TAT::Legs(std::to_string(i));
+}
+
+Tensor calc_total(const std::vector<Tensor>& lattice) {
+  Tensor res;
+  res = lattice[0].contract(lattice[1], {Right}, {Left}, {{Phy, get_leg(0)}}, {{Phy, get_leg(1)}});
+  for(unsigned long i=2; i<lattice.size()-1; i++) {
+    res = res.contract(lattice[i], {Right}, {Left}, {}, {{Phy, get_leg(i)}});
+  }
+  res = Tensor::contract(res, lattice[lattice.size()-1], {Right, Left}, {Left, Right}, {}, {{Phy, get_leg(lattice.size()-1)}});
+  return res;
+}
+
 int cut_by_svd(int L=4, unsigned long D=4, unsigned long d=4, unsigned long cut=3) {
+  // init
   std::vector<Tensor> lattice;
   std::srand(0);
   for (int i=0; i<L; i++) {
@@ -53,16 +68,12 @@ int cut_by_svd(int L=4, unsigned long D=4, unsigned long d=4, unsigned long cut=
     if (i==L-1) r=1;
     lattice.push_back(std::move(Tensor({Left, Right, Phy}, {l, r, d}).set_random(get_random)));
   }
+  // print
   std::cout << "origin data is" << std::endl << lattice << std::endl;
-  auto origin = lattice[0];
-  origin.legs_rename({{Phy, TAT::Legs(std::to_string(0))}});
-  for (int i=1; i<L-1; i++) {
-    origin = origin.contract(lattice[i], {Right}, {Left}, {}, {{Phy, TAT::Legs(std::to_string(i))}});
-  }
-  origin = Tensor::contract(origin, lattice[L-1], {Right, Left}, {Left, Right}, {}, {{Phy, TAT::Legs(std::to_string(L-1))}});
+  auto origin = calc_total(lattice);
   std::cout << "origin total vector is" << std::endl;
   std::cout << origin << std::endl << origin.norm<-1>() << std::endl << std::endl;
-  auto bak = std::move(origin);
+  // update
   for (int i=0; i<L-1; i++) {
     auto qr = lattice[i].qr({Left, Phy}, Right, Left);
     lattice[i] = std::move(qr.Q);
@@ -73,18 +84,14 @@ int cut_by_svd(int L=4, unsigned long D=4, unsigned long d=4, unsigned long cut=
     lattice[i] = std::move(svd.V);
     lattice[i-1] = lattice[i-1].contract(svd.U, {Right}, {Left}).multiple(svd.S, {Right});
   }
+  // print
   std::cout << "cut data is" << std::endl << lattice << std::endl;
-  origin = lattice[0];
-  origin.legs_rename({{Phy, TAT::Legs(std::to_string(0))}});
-  for (int i=1; i<L-1; i++) {
-    origin = origin.contract(lattice[i], {Right}, {Left}, {}, {{Phy, TAT::Legs(std::to_string(i))}});
-  }
-  origin = Tensor::contract(origin, lattice[L-1], {Right, Left}, {Left, Right}, {}, {{Phy, TAT::Legs(std::to_string(L-1))}});
+  auto after_cut = calc_total(lattice);
   std::cout << "total vector after cut is" << std::endl;
-  std::cout << origin << std::endl << origin.norm<-1>() << std::endl << std::endl;
-  origin = origin - bak;
+  std::cout << after_cut << std::endl << after_cut.norm<-1>() << std::endl << std::endl;
+  auto diff = after_cut - origin;
   std::cout << "diff is" << std::endl;
-  std::cout << origin << std::endl << origin.norm<-1>() << std::endl << std::endl;
+  std::cout << diff << std::endl << diff.norm<-1>() << std::endl << std::endl;
   return 0;
 }
 
