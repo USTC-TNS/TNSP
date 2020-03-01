aacb5ea4250eb630f62ff3e3ea617be1ad445a66
Hao Zhang <zh970205@mail.ustc.edu.cn>
2020-03-01 21:12:41 +0800

    给edge_operator添加了注释

--- include/TAT/edge_operator.hpp
+++ include/TAT/edge_operator.hpp
@@ -45,29 +45,33 @@ namespace TAT {
       // parity_exclude_name的四部分分别是split, reverse, reverse_for_merge, merge
       // name 均为rename后的名称，split取split前， merge取merge后
 
-      using ConstDataType = std::map<vector<Symmetry>, const ScalarType*>;
-      using DataType = std::map<vector<Symmetry>, ScalarType*>;
-
-      // status 0
+      // is_fermi
+      constexpr auto is_fermi = is_fermi_symmetry_v<Symmetry>;
+      // status 0 origin
+      // rank_0 and name_0
       const auto rank_0 = names.size();
       const auto& name_0 = names;
+      // create edge 0
       auto edge_0 = vector<PtrEdge<Symmetry>>();
       for (const auto& e : core->edges) {
-         if constexpr (is_fermi_symmetry_v<Symmetry>) {
+         if constexpr (is_fermi) {
             edge_0.push_back({e.arrow, &e.map});
          } else {
             edge_0.push_back({&e.map});
          }
       }
-      auto data_0 = ConstDataType();
+      // create data_0
+      auto data_0 = std::map<vector<Symmetry>, const ScalarType*>();
       for (const auto& [sym, vec] : core->blocks) {
          data_0[sym] = vec.data();
       }
 
       // status 1 rename
+      // rank_1 and edge_1 and data_1
       const auto& rank_1 = rank_0;
       const auto& edge_1 = edge_0;
       const auto& data_1 = data_0;
+      // create name_1
       auto true_name_1 = vector<Name>();
       auto ptr_name_1 = &name_0;
       if (!rename_map.empty()) {
@@ -86,7 +90,7 @@ namespace TAT {
 
       // check no change
       if (name_1 == new_names && reversed_name.empty() && split_map.empty() && merge_map.empty()) {
-         // 共享核心
+         // share the core
          auto res = Tensor<ScalarType, Symmetry>();
          res.names = std::forward<T>(new_names);
          res.name_to_index = construct_name_to_index(res.names);
@@ -95,19 +99,20 @@ namespace TAT {
       }
 
       // status 2 split
+      // create name_2 and edge_2 and split_flag
       auto split_flag = vector<Rank>();
-      Rank total_split_index = 0;
       auto true_name_2 = vector<Name>();
       auto true_edge_2 = vector<PtrEdge<Symmetry>>();
       auto ptr_name_2 = &name_1;
       auto ptr_edge_2 = &edge_1;
       if (!split_map.empty()) {
+         Rank total_split_index = 0;
          for (auto i = 0; i < rank_1; i++) {
             auto pos = split_map.find(name_1[i]);
             if (pos != split_map.end()) {
                for (const auto& [n, e] : pos->second) {
                   true_name_2.push_back(n);
-                  if constexpr (is_fermi_symmetry_v<Symmetry>) {
+                  if constexpr (is_fermi) {
                      true_edge_2.push_back({edge_1[i].arrow, &e.map});
                   } else {
                      true_edge_2.push_back({&e.map});
@@ -131,9 +136,10 @@ namespace TAT {
       }
       const auto& name_2 = *ptr_name_2;
       const auto& edge_2 = *ptr_edge_2;
-
+      // rank_2
       const auto rank_2 = name_2.size();
-      auto true_data_2 = ConstDataType();
+      // create data_2
+      auto true_data_2 = std::map<vector<Symmetry>, const ScalarType*>();
       auto ptr_data_2 = &data_1;
       if (!split_map.empty()) {
          auto offset_src = data_1;
@@ -153,26 +159,16 @@ namespace TAT {
       }
       const auto& data_2 = *ptr_data_2;
 
-      [[maybe_unused]] auto split_flag_mark = vector<bool>();
-      if constexpr (is_fermi_symmetry_v<Symmetry>) {
-         if (apply_parity) {
-            for (auto i = 0; i < rank_1; i++) {
-               // 不相等 => 被exclude => 不要应用parity
-               split_flag_mark[i] =
-                     parity_exclude_name[0].find(name_1[i]) != parity_exclude_name[0].end();
-            }
-         }
-      }
-
       // status 3 reverse
-      [[maybe_unused]] auto reversed_flag_src = vector<bool>();
-      [[maybe_unused]] auto reversed_flag_src_mark = vector<bool>();
+      // rank_3 and name_3 and data_3
       const auto& rank_3 = rank_2;
       const auto& name_3 = name_2;
       const auto& data_3 = data_2;
+      // create reversed_flag_src and edge_3
+      auto reversed_flag_src = vector<bool>();
       auto ptr_edge_3 = &edge_2;
       auto true_edge_3 = vector<PtrEdge<Symmetry>>();
-      if constexpr (is_fermi_symmetry_v<Symmetry>) {
+      if constexpr (is_fermi) {
          if (!reversed_name.empty()) {
             reversed_flag_src.resize(rank_3);
             true_edge_3 = edge_2;
@@ -193,40 +189,23 @@ namespace TAT {
          }
       }
       const auto& edge_3 = *ptr_edge_3;
-      if constexpr (is_fermi_symmetry_v<Symmetry>) {
-         if (apply_parity) {
-            for (auto i = 0; i < rank_2; i++) {
-               // 不相等 => 被exclude => 不要应用parity
-               reversed_flag_src_mark[i] =
-                     parity_exclude_name[1].find(name_2[i]) != parity_exclude_name[1].end();
-            }
-         }
-      }
 
+      // create res names
       auto res = Tensor<ScalarType, Symmetry>();
       res.names = std::forward<T>(new_names);
       res.name_to_index = construct_name_to_index(res.names);
       // status 4 transpose and status 6 merge and status 5 reverse before merge
-      // 先根据merge何name_6确定name_4, 再确定根据edge_3确定edge_4
+      // 逆序确定name, 再顺序确定edge和data
       // name and rank
+      // name_6 and rank_6
       const auto& name_6 = res.names;
       const auto& rank_6 = name_6.size();
-
+      // create merge_flag and name_4
       auto merge_flag = vector<Rank>();
-      auto merge_flag_mark = vector<bool>();
-      if constexpr (is_fermi_symmetry_v<Symmetry>) {
-         if (apply_parity) {
-            for (auto i = 0; i < rank_6; i++) {
-               // 不相等 => 被exclude => 不要应用parity
-               merge_flag_mark[i] =
-                     parity_exclude_name[3].find(name_6[i]) != parity_exclude_name[3].end();
-            }
-         }
-      }
-      Rank total_merge_index = 0;
       auto true_name_4 = vector<Name>();
       auto ptr_name_4 = &name_6;
       if (!merge_map.empty()) {
+         Rank total_merge_index = 0;
          for (const auto& n : name_6) {
             auto pos = merge_map.find(n);
             if (pos != merge_map.end()) {
@@ -248,14 +227,18 @@ namespace TAT {
          }
       }
       const auto& name_4 = *ptr_name_4;
+      // rank_4 and name_5 and rank_5
       const auto rank_4 = name_4.size();
       const auto& name_5 = name_4;
       const auto& rank_5 = rank_4;
 
-      // edge
+      // to be easy, create name_to_index for name_3
       auto name_to_index_3 = construct_name_to_index(name_3);
+      // create plan of two way
       auto plan_src_to_dst = vector<Rank>(rank_4);
       auto plan_dst_to_src = vector<Rank>(rank_4);
+      // edge
+      // create edge_4
       auto edge_4 = vector<PtrEdge<Symmetry>>(rank_4);
       for (auto i = 0; i < rank_4; i++) {
          plan_dst_to_src[i] = name_to_index_3.at(name_4[i]);
@@ -263,40 +246,39 @@ namespace TAT {
          edge_4[i] = edge_3[plan_dst_to_src[i]];
       }
 
-      [[maybe_unused]] auto reversed_flag_dst = vector<bool>();
-      [[maybe_unused]] auto reversed_flag_dst_mark = vector<bool>();
-      if constexpr (is_fermi_symmetry_v<Symmetry>) {
-         if (apply_parity) {
-            for (auto i = 0; i < rank_5; i++) {
-               // 不相等 => 被exclude => 不要应用parity
-               reversed_flag_dst_mark[i] =
-                     parity_exclude_name[2].find(name_5[i]) != parity_exclude_name[2].end();
-            }
-         }
-      }
-      auto res_edge = vector<Edge<Symmetry>>();
-      auto start_of_merge = 0;
-      auto end_of_merge = 0;
+      // the following code is about merge
+      // dealing with edge_5 and res_edge and reversed_flag_dst
+
+      // prepare edge_5
+      // if no merge, edge_5 is reference of edge_4, else is copy of edge_4
       auto ptr_edge_5 = &edge_4;
       auto true_edge_5 = vector<PtrEdge<Symmetry>>();
       if (!merge_map.empty()) {
-         if constexpr (is_fermi_symmetry_v<Symmetry>) {
+         if constexpr (is_fermi) {
             true_edge_5 = edge_4;
             ptr_edge_5 = &true_edge_5;
          }
       }
       auto& edge_5 = *ptr_edge_5;
-
+      // prepare res_edge
+      // res_edge means edge_6 but type is different
+      auto res_edge = vector<Edge<Symmetry>>();
+      // prepare reversed_flag_dst
+      auto reversed_flag_dst = vector<bool>();
       if (!merge_map.empty()) {
+         // edge_5 is copy of edge_4 and here is will change
+         auto start_of_merge = 0;
+         auto end_of_merge = 0;
          for (auto i = 0; i < rank_6; i++) {
+            // [start, end) need be merged
             while (end_of_merge < rank_4 && merge_flag[end_of_merge] == i) {
                end_of_merge++;
             }
-            [[maybe_unused]] Arrow arrow;
-            [[maybe_unused]] bool arrow_fixed = false;
+            Arrow arrow;
+            bool arrow_fixed = false;
             auto edge_to_merge = vector<PtrEdge<Symmetry>>();
             for (auto j = start_of_merge; j < end_of_merge; j++) {
-               if constexpr (is_fermi_symmetry_v<Symmetry>) {
+               if constexpr (is_fermi) {
                   if (edge_5[j].arrow_valid()) {
                      if (arrow_fixed) {
                         if (arrow == edge_5[j].arrow) {
@@ -317,38 +299,44 @@ namespace TAT {
                edge_to_merge.push_back(edge_5[j]);
             }
             auto merged_edge = get_merged_edge(edge_to_merge);
-            if constexpr (is_fermi_symmetry_v<Symmetry>) {
+            if constexpr (is_fermi) {
                merged_edge.arrow = arrow;
             }
             res_edge.push_back(std::move(merged_edge));
             start_of_merge = end_of_merge;
          }
       } else {
+         // if no merge, res_edge is just edge_5, and reversed_flag_dst is all false
+         // edge_5 is reference of edge_4 since no reverse
          res_edge.resize(rank_4);
          reversed_flag_dst.resize(rank_4);
          for (auto i = 0; i < rank_4; i++) {
             reversed_flag_dst[i] = false;
-            if constexpr (is_fermi_symmetry_v<Symmetry>) {
+            if constexpr (is_fermi) {
                res_edge[i] = {edge_4[i].arrow, *edge_4[i].map};
             } else {
                res_edge[i] = {*edge_4[i].map};
             }
          }
       }
+      // the code above is dealing with edge_5 and res_Edge and reversed_flag_dst
 
+      // put res_edge into res
       res.core = std::make_shared<Core<ScalarType, Symmetry>>(std::move(res_edge));
       if (!is_valid_name(res.names, res.core->edges.size())) {
          TAT_WARNING("Invalid Names");
       }
+      // edge_6
       const auto& edge_6 = res.core->edges;
 
       // data
-      auto data_6 = DataType();
+      // data_6
+      auto data_6 = std::map<vector<Symmetry>, ScalarType*>();
       for (auto& [sym, vec] : res.core->blocks) {
          data_6[sym] = vec.data();
       };
-
-      auto true_data_5 = DataType();
+      // create data_5
+      auto true_data_5 = std::map<vector<Symmetry>, ScalarType*>();
       auto ptr_data_5 = &data_6;
       if (!merge_map.empty()) {
          auto offset_dst = data_6;
@@ -367,8 +355,39 @@ namespace TAT {
          ptr_data_5 = &true_data_5;
       }
       const auto& data_5 = *ptr_data_5;
+      // data_4
       const auto& data_4 = data_5;
 
+      // 4 marks
+      auto split_flag_mark = vector<bool>();
+      auto reversed_flag_src_mark = vector<bool>();
+      auto reversed_flag_dst_mark = vector<bool>();
+      auto merge_flag_mark = vector<bool>();
+      if constexpr (is_fermi) {
+         if (apply_parity) {
+            split_flag_mark.resize(rank_1);
+            for (auto i = 0; i < rank_1; i++) {
+               split_flag_mark[i] =
+                     parity_exclude_name[0].find(name_1[i]) != parity_exclude_name[0].end();
+            }
+            reversed_flag_src_mark.resize(rank_2);
+            for (auto i = 0; i < rank_2; i++) {
+               reversed_flag_src_mark[i] =
+                     parity_exclude_name[1].find(name_2[i]) != parity_exclude_name[1].end();
+            }
+            reversed_flag_dst_mark.resize(rank_5);
+            for (auto i = 0; i < rank_5; i++) {
+               reversed_flag_dst_mark[i] =
+                     parity_exclude_name[2].find(name_5[i]) != parity_exclude_name[2].end();
+            }
+            merge_flag_mark.resize(rank_6);
+            for (auto i = 0; i < rank_6; i++) {
+               merge_flag_mark[i] =
+                     parity_exclude_name[3].find(name_6[i]) != parity_exclude_name[3].end();
+            }
+         }
+      }
+      // main copy loop
       for (const auto& [sym_src, ptr_src] : data_3) {
          auto sym_dst = vector<Symmetry>(rank_3);
          auto dim_src = vector<Size>(rank_3);
@@ -384,7 +403,7 @@ namespace TAT {
          auto ptr_dst = data_4.at(sym_dst);
 
          bool parity = false;
-         if constexpr (is_fermi_symmetry_v<Symmetry>) {
+         if constexpr (is_fermi) {
             parity = Symmetry::get_transpose_parity(sym_src, plan_src_to_dst);
 
             if (apply_parity) {
