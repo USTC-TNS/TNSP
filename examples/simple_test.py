377e914f4dbe628f6c9871bdb9ffba0e3be855f2
Hao Zhang <hzhangxyz@outlook.com>
2020-08-30 21:33:29 +0800

    在simple_test.py中继续增加测试

--- examples/simple_test.py
+++ examples/simple_test.py
@@ -67,8 +67,8 @@ def test_norm():
 def test_scalar():
     t = TAT.Tensor.DZ2(["Left", "Right", "Phy"], [{0: 2, 1: 2}, {0: 2, 1: 2}, {0: 2, 1: 2}])
     t.test()
-    print(t+1.0)
-    print(1.0/t)
+    print(t + 1.0)
+    print(1.0 / t)
 
     a = TAT.Tensor.DNo(["Left", "Right"], [3, 4]).test()
     b = TAT.Tensor.DNo(["Left", "Right"], [3, 4]).test(0, 0.1)
@@ -103,6 +103,68 @@ def test_io():
     print(j)
 
 
+def test_edge_rename():
+    t1 = TAT.Tensor.DZ2(["Left", "Right", "Phy"], [{0: 1, 1: 2}, {0: 3, 1: 4}, {0: 5, 1: 6}])
+    t2 = t1.edge_rename({"Left": "Up"})
+    t1.test()
+    print(t1)
+    print(t2)
+
+
+def test_transpose():
+    a = TAT.Tensor.DNo(["Left", "Right"], [2, 3]).test()
+    print(a)
+    print(a.transpose(["Right", "Left"]))
+
+    b = TAT.Tensor.DNo(["Left", "Right", "Up"], [2, 3, 4]).test()
+    print(b)
+    print(b.transpose(["Right", "Up", "Left"]))
+
+    c = TAT.Tensor.ZU1(["Left", "Right", "Up"], [{-1: 3, 0: 1, 1: 2}, {-1: 1, 0: 2, 1: 3}, {-1: 2, 0: 3, 1: 1}]).test(1)
+    print(c)
+    ct = c.transpose(["Right", "Up", "Left"])
+    print(ct)
+    print(c[{"Left": (-1, 0), "Right": (1, 2), "Up": (0, 0)}])
+    print(ct[{"Left": (-1, 0), "Right": (1, 2), "Up": (0, 0)}])
+
+    d = TAT.Tensor.DFermi(["Left", "Right", "Up"], [{-1: 3, 0: 1, 1: 2}, {-1: 1, 0: 2, 1: 3}, {-1: 2, 0: 3, 1: 1}], True) .test(1)
+    print(d)
+    dt = d.transpose(["Right", "Up", "Left"])
+    print(dt)
+
+    e = TAT.Tensor.DNo(["Down", "Up", "Left", "Right"], [2, 3, 4, 5]).test(1)
+    print(e)
+    et = e.transpose(["Left", "Down", "Right", "Up"])
+    print(et)
+
+    f = TAT.Tensor.DNo(["l1", "l2", "l3"], [2, 3, 4]).test()
+    print(f)
+    print(f.transpose(["l1", "l2", "l3"]))
+    print(f.transpose(["l1", "l3", "l2"]))
+    print(f.transpose(["l2", "l1", "l3"]))
+    print(f.transpose(["l2", "l3", "l1"]))
+    print(f.transpose(["l3", "l1", "l2"]))
+    print(f.transpose(["l3", "l2", "l1"]))
+
+
+def test_split_and_merge():
+    class initializer():
+        def __init__(self):
+            self.i = -1
+
+        def __call__(self):
+            self.i += 1
+            return self.i
+    a = TAT.Tensor.DNo(["Left", "Right"], [2, 3]).set(initializer())
+    b = a.merge_edge({"Merged": ["Left", "Right"]})
+    c = a.merge_edge({"Merged": ["Right", "Left"]})
+    # d = c.split_edge({"Merged": [("1", 3), ("2", 2)]});
+    print(a)
+    print(b)
+    print(c)
+    # print(d)
+
+
 if __name__ == "__main__":
     run_test(test_create_tensor)
     run_test(test_create_symmetry_tensor)
@@ -111,3 +173,6 @@ if __name__ == "__main__":
     run_test(test_norm)
     run_test(test_scalar)
     run_test(test_io)
+    run_test(test_edge_rename)
+    run_test(test_transpose)
+    run_test(test_split_and_merge)
