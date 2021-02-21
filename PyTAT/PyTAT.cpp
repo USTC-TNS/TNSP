d0d198103bc73ff3ed27b7f4c8598d649fbce61f
Hao Zhang <hzhangxyz@outlook.com>
2021-02-21 19:55:43 +0800

    split something out of PyTAT.cpp

--- PyTAT/PyTAT.cpp
+++ PyTAT/PyTAT.cpp
@@ -18,6 +18,10 @@
 #include "PyTAT.hpp"
 
 namespace TAT {
+   void set_navigator(py::module_&);
+   void set_random(py::module_&);
+   py::class_<mpi_t> set_mpi(py::module_&);
+   void set_name(py::module_&);
 #define TAT_SINGLE_SCALAR_SYMMETRY(SCALARSHORT, SCALAR, SYM)                                                                                     \
    void dealing_Tensor_##SCALARSHORT##SYM(                                                                                                       \
          py::module_& symmetry_m, const std::string& scalar_short_name, const std::string& scalar_name, const std::string& symmetry_short_name); \
@@ -34,82 +38,16 @@ namespace TAT {
       tat_m.attr("version") = version;
       tat_m.attr("information") = information;
       // random
-      auto random_m = tat_m.def_submodule("random", "random for TAT");
-      random_m.def("seed", &set_random_seed, "Set Random Seed");
-      random_m.def(
-            "uniform_int",
-            [](int min, int max) {
-               return [distribution = std::uniform_int_distribution<int>(min, max)]() mutable { return distribution(random_engine); };
-            },
-            py::arg("min") = 0,
-            py::arg("max") = 1,
-            "Get random uniform integer");
-      random_m.def(
-            "uniform_real",
-            [](double min, double max) {
-               return [distribution = std::uniform_real_distribution<double>(min, max)]() mutable { return distribution(random_engine); };
-            },
-            py::arg("min") = 0,
-            py::arg("max") = 1,
-            "Get random uniform real");
-      random_m.def(
-            "normal",
-            [](double mean, double stddev) {
-               return [distribution = std::normal_distribution<double>(mean, stddev)]() mutable { return distribution(random_engine); };
-            },
-            py::arg("mean") = 0,
-            py::arg("stddev") = 1,
-            "Get random normal real");
+      set_random(tat_m);
       // mpi
-      auto py_mpi_t = py::class_<mpi_t>(tat_m, "mpi_t", "several functions for MPI")
-                            .def_readonly_static("enabled", &mpi_t::enabled)
-                            .def_readonly("rank", &mpi_t::rank)
-                            .def_readonly("size", &mpi_t::size)
-                            .def("__str__",
-                                 [](const mpi_t& mpi) {
-                                    auto out = std::stringstream();
-                                    out << "[rank=" << mpi.rank << ", size=" << mpi.size << "]";
-                                    return out.str();
-                                 })
-                            .def("__repr__",
-                                 [](const mpi_t& mpi) {
-                                    auto out = std::stringstream();
-                                    out << "MPI[rank=" << mpi.rank << ", size=" << mpi.size << "]";
-                                    return out.str();
-                                 })
-                            .def("print",
-                                 [](const mpi_t& mpi, const py::args& args, const py::kwargs& kwargs) {
-                                    if (mpi.rank == 0) {
-                                       py::print(*args, **kwargs);
-                                    }
-                                 })
-#ifdef TAT_USE_MPI
-                            .def_static("barrier", &mpi_t::barrier)
-#endif
-            ;
+      auto py_mpi_t = set_mpi(tat_m);
 #define TAT_SINGLE_SCALAR_SYMMETRY(SCALARSHORT, SCALAR, SYM) dealing_MPI_##SCALARSHORT##SYM(py_mpi_t);
       TAT_LOOP_ALL_SCALAR_SYMMETRY
 #undef TAT_SINGLE_SCALAR_SYMMETRY
       tat_m.attr("mpi") = mpi;
       // name
-#ifndef TAT_USE_SIMPLE_NAME
-      py::class_<DefaultName>(tat_m, "Name", "Name used in edge of tensor, which is just a string but stored by identical integer")
-            .def(py::self < py::self)
-            .def(py::self > py::self)
-            .def(py::self <= py::self)
-            .def(py::self >= py::self)
-            .def(py::self == py::self)
-            .def(py::self != py::self)
-            .def(py::init<fastname_dataset_t::fast_name_id_t>(), py::arg("id"), "Name with specified id directly")
-            .def_readonly("id", &DefaultName::id)
-            .def("__hash__", [](const DefaultName& name) { return py::hash(py::cast(name.id)); })
-            .def_static("load", [](const py::bytes& bytes) { return load_fastname_dataset(std::string(bytes)); })
-            .def_static("dump", []() { return py::bytes(dump_fastname_dataset()); })
-            .def_property_readonly("name", [](const DefaultName& name) { return static_cast<const std::string&>(name); })
-            .def(implicit_init<DefaultName, const char*>(), py::arg("name"), "Name with specified name")
-            .def("__repr__", [](const DefaultName& name) { return "Name[" + static_cast<const std::string&>(name) + "]"; })
-            .def("__str__", [](const DefaultName& name) { return static_cast<const std::string&>(name); });
-#endif
+      set_name(tat_m);
+      // symmetry and edge
       auto No_m = tat_m.def_submodule("No");
       declare_symmetry<NoSymmetry>(No_m, "No").def(py::init<>());
       declare_edge<NoSymmetry, void, false>(No_m, "No");
@@ -157,76 +95,9 @@ namespace TAT {
       TAT_LOOP_ALL_SCALAR_SYMMETRY
 #undef TAT_SINGLE_SCALAR_SYMMETRY
       // get tensor
-      tat_m.def("navigator", [tat_m](const py::args& args, const py::kwargs& kwargs) -> py::object {
-         if (py::len(args) == 0 && py::len(kwargs) == 0) {
-            tat_m.attr("mpi").attr("print")(information);
-            return py::none();
-         }
-         auto text = py::str(py::make_tuple(args, kwargs));
-         auto contain = [&text](const char* string) { return py::cast<bool>(text.attr("__contains__")(string)); };
-         if (contain("mpi") || contain("MPI")) {
-            return tat_m.attr("mpi");
-         }
-         std::string scalar = "";
-         std::string fermi = "";
-         std::string symmetry = "";
-         if (contain("Fermi")) {
-            fermi = "Fermi";
-         }
-         if (contain("Bose")) {
-            if (fermi != "") {
-               throw std::runtime_error("Fermi Ambiguous");
-            }
-         }
-         if (contain("U1")) {
-            symmetry = "U1";
-         }
-         if (contain("Z2")) {
-            if (symmetry == "") {
-               symmetry = "Z2";
-            } else {
-               throw std::runtime_error("Symmetry Ambiguous");
-            }
-         }
-         if (contain("No")) {
-            if (symmetry != "") {
-               throw std::runtime_error("Symmetry Ambiguous");
-            }
-         }
-         if (symmetry == "" && fermi == "") {
-            symmetry = "No";
-         }
-         if (contain("complex")) {
-            scalar = "Z";
-         }
-         if (contain("complex32")) {
-            scalar = "C";
-         }
-         if (contain("float")) {
-            if (scalar == "") {
-               scalar = "D";
-            } else {
-               throw std::runtime_error("Scalar Ambiguous");
-            }
-         }
-         if (contain("float32")) {
-            if (scalar == "" || scalar == "D") {
-               scalar = "S";
-            } else {
-               throw std::runtime_error("Scalar Ambiguous");
-            }
-         }
-         if (scalar == "") {
-            throw std::runtime_error("Scalar Ambiguous");
-         }
-         return tat_m.attr((fermi + symmetry).c_str()).attr(scalar.c_str()).attr("Tensor");
-      });
-      auto py_type = py::module_::import("builtins").attr("type");
-      py::dict callable_type_dict;
-      callable_type_dict["__call__"] = tat_m.attr("navigator");
-      py::list base_types;
-      base_types.append(py::type::of(tat_m));
-      tat_m.attr("__class__") = py_type("CallableModule", py::tuple(base_types), callable_type_dict);
+      set_navigator(tat_m);
+      // normal = no
+      tat_m.attr("Normal") = tat_m.attr("No");
       at_exit.release();
    }
 } // namespace TAT
