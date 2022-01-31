b2c89023c8dbaf1d95555a08a65fddaeb9de2954
Hao Zhang <hzhangxyz@outlook.com>
2022-01-31 15:43:34 +0800

    [build] setup.py call cmake to build PyTAT

--- PyTAT/pyodide/PyTAT/setup.py
+++ PyTAT/pyodide/PyTAT/setup.py
@@ -1,14 +1,42 @@
-import os.path
-from glob import glob
+import os
+import sys
+import pathlib
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as build_ext_original
 
-ext_modules = [
-    Extension("TAT",
-              sorted([*glob(os.path.join("PyTAT", "*.cpp")), *glob(os.path.join("PyTAT", "generated_code", "*.cpp"))]),
-              include_dirs=["include", "pybind11/include"],
-              language="c++",
-              extra_compile_args=["-std=c++17", "-g0"]),
-]
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name, sources=[]):
+        super().__init__(name=name, sources=sources)
+
+
+class build_ext(build_ext_original):
+
+    def run(self):
+        for extension in self.extensions:
+            self.build_cmake(extension)
+
+    def build_cmake(self, extension):
+        cwd = pathlib.Path().absolute()
+        build_dir = pathlib.Path(self.build_temp)
+        build_dir.mkdir(parents=True, exist_ok=True)
+        extension_dir = pathlib.Path(self.get_ext_fullpath(extension.name)).parent
+        extension_dir.mkdir(parents=True, exist_ok=True)
+
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extension_dir.absolute()),
+            "-DCMAKE_BUILD_TYPE=" + "Release",
+            "-DTAT_USE_MPI=" + "OFF",
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
+        os.chdir(str(build_dir))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+
+        if not self.dry_run:
+            self.spawn(["cmake", "--build", ".", "--target", extension.name, "--parallel", "4"])
+        os.chdir(str(cwd))
+
 
 setup(
     name="PyTAT",
@@ -17,7 +45,10 @@ setup(
     author="Hao Zhang",
     author_email="zh970205@mail.ustc.edu.cn",
     url="https://github.com/hzhangxyz/TAT",
-    ext_modules=ext_modules,
+    ext_modules=[CMakeExtension("PyTAT")],
+    cmdclass={
+        'build_ext': build_ext,
+    },
     install_requires=[
         "numpy",
     ],
