272f7f5e1dfe478538c49cd220242ed4a6f30430
Chao Wang <1023649157@qq.com>
2022-03-17 18:29:22 +0800

    [example] add Sz measurement for bosen metal.

---
+++ examples/tetra_models/boson_metal/Sz.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 Chao Wang<1023649157@qq.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import TAT
+import tetragono as tet
+
+
+def measurement(state):
+    Sz = tet.common_variable.No.Sz.to(float)
+    result = {((l1, l2, 0),): Sz for l1 in range(state.L1) for l2 in range(state.L2)}
+    return result
+
+
+def save_result(state, result, step):
+    to_print = [result[(l1, l2, 0),][0] for l1 in range(state.L1) for l2 in range(state.L2)]
+    with open("Sz.log", "a") as file:
+        print(*to_print, file=file)
