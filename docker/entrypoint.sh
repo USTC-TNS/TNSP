1cd3e3f5e3f30aa5d47352843a6b412ac6ccac7b
Hao Zhang <hzhangxyz@outlook.com>
2022-03-18 23:35:55 +0800

    [build] use volume binding instead of clone from remote repo in docker.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo args is $@
+echo usage example: docker run -it --rm -v $PWD:/TAT -v /TAT/build image_name python3.10 PyTAT
 cd TAT
-$1 setup/$2.py bdist_wheel -p manylinux2014_x86_64 -d /dist
+$1 setup/$2.py bdist_wheel -p manylinux2014_x86_64
