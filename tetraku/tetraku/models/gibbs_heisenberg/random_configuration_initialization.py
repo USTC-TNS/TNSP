b0f41d82e73cac60d38544b19e044ffcb18e7431
Hao Zhang <hzhangxyz@outlook.com>
2023-03-28 00:09:57 +0800

    [tetku.py] Add random initialize for configuration of gibbs heisenberg model.

---
+++ tetraku/tetraku/models/gibbs_heisenberg/random_configuration_initialization.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022-2023 Chao Wang<1023649157@qq.com>
+# Copyright (C) 2023 Hao Zhang<zh970205@mail.ustc.edu.cn>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import TAT
+import tetragono as tet
+
+
+def initial_configuration(configuration):
+    state = configuration.owner
+    random = TAT.random.uniform_int(0, 1)
+    for l1, l2 in state.sites():
+        configuration[l1, l2, 0] = configuration[l1, l2, 1] = random()
+    return configuration
