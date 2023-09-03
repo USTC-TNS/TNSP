52c86fd9865d32854c505790c66a4d4b216ac348
Hao Zhang <hzhangxyz@outlook.com>
2023-09-03 15:32:08 +0800

    [test, TAT.hpp] Add tests for elementwise operator of tensor.

---
+++ tests/test_norm.cpp
@@ -0,0 +1,53 @@
+#include <TAT/TAT.hpp>
+#include <gtest/gtest.h>
+
+TEST(test_norm, basic_usage) {
+   auto t = (TAT::Tensor<double, TAT::U1Symmetry>{
+         {"Left", "Right", "Up"},
+         {{{-1, 3}, {0, 1}, {1, 2}}, {{-1, 1}, {0, 2}, {1, 3}}, {{-1, 2}, {0, 3}, {1, 1}}}}
+                   .range(7)
+                   .to<std::complex<double>>());
+   //  0  0  0 -> 1 2 3 -> 6
+   //  0 +1 -1 -> 1 3 2 -> 6
+   //  0 -1 +1 -> 1 1 1 -> 1
+   // +1  0 -1 -> 2 2 2 -> 8
+   // -1  0 +1 -> 3 2 1 -> 6
+   // +1 -1  0 -> 2 1 3 -> 6
+   // -1 +1  0 -> 3 3 3 -> 27
+   // total: 60
+   // from 7 to 66
+   ASSERT_EQ(t.storage().size(), 60);
+   ASSERT_FLOAT_EQ(t.norm<-1>(), 66);
+   ASSERT_FLOAT_EQ(t.norm<0>(), 60);
+   ASSERT_FLOAT_EQ(t.norm<1>(), (7 + 66) * 60 / 2);
+   ASSERT_FLOAT_EQ(t.norm<2>(), std::pow((66 * (66 + 1) * (2 * 66 + 1) - 6 * (6 + 1) * (2 * 6 + 1)) / 6., 1 / 2.));
+}
+
+TEST(test_norm, rank3) {
+   auto t = (TAT::Tensor<double, TAT::U1Symmetry>{
+         {"Left", "Right", "Up"},
+         {{{-1, 3}, {0, 1}, {1, 2}}, {{-1, 1}, {0, 2}, {1, 3}}, {{-1, 2}, {0, 3}, {1, 1}}}}
+                   .range(7)
+                   .to<std::complex<double>>());
+   double expect = 0;
+   constexpr int k = 3;
+   for (int i = 7; i < 67; i++) {
+      expect += std::pow(i, k);
+   }
+   expect = std::pow(expect, 1. / k);
+   ASSERT_FLOAT_EQ(t.norm<k>(), expect);
+}
+
+TEST(test_norm, rank4) {
+   auto t = (TAT::Tensor<double, TAT::U1Symmetry>{
+         {"Left", "Right", "Up"},
+         {{{-1, 3}, {0, 1}, {1, 2}}, {{-1, 1}, {0, 2}, {1, 3}}, {{-1, 2}, {0, 3}, {1, 1}}}}
+                   .range(7));
+   double expect = 0;
+   constexpr int k = 4;
+   for (int i = 7; i < 67; i++) {
+      expect += std::pow(i, k);
+   }
+   expect = std::pow(expect, 1. / k);
+   ASSERT_FLOAT_EQ(t.norm<k>(), expect);
+}
---
+++ tests/test_transform_map_and_set.cpp
@@ -0,0 +1,95 @@
+#include <TAT/TAT.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+TEST(test_transform_map_and_set, transform) {
+   auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(); // 0, 1, 2, 3, 4, 5
+   ASSERT_THAT(t.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+   t.transform([](auto x) {
+      return x + 1;
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+}
+
+TEST(test_transform_map_and_set, transform_rvalue) {
+   auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range().transform([](auto x) {
+      return x + 1;
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+}
+
+TEST(test_transform_map_and_set, zip_transform) {
+   auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(0); // 0, 1, 2, 3, 4, 5
+   auto s = TAT::Tensor<>({"i", "j"}, {2, 3}).range(1); // 1, 2, 3, 4, 5, 6
+   ASSERT_THAT(t.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+   ASSERT_THAT(s.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+   t.zip_transform(s, [](auto it, auto is) {
+      return is - it;
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(1, 1, 1, 1, 1, 1));
+}
+
+TEST(test_transform_map_and_set, zip_transform_rvalue) {
+   auto s = TAT::Tensor<>({"i", "j"}, {2, 3}).range(1); // 1, 2, 3, 4, 5, 6
+   ASSERT_THAT(s.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+   auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(0).zip_transform(s, [](auto it, auto is) {
+      return is - it;
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(1, 1, 1, 1, 1, 1));
+}
+
+TEST(test_transform_map_and_set, map) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(); // 0, 1, 2, 3, 4, 5
+   ASSERT_THAT(t.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+   const auto z = t.map([](auto x) {
+      return x + 1;
+   });
+   ASSERT_THAT(z.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+}
+
+TEST(test_transform_map_and_set, zip_map) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(0); // 0, 1, 2, 3, 4, 5
+   const auto s = TAT::Tensor<>({"i", "j"}, {2, 3}).range(1); // 1, 2, 3, 4, 5, 6
+   ASSERT_THAT(t.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+   ASSERT_THAT(s.storage(), ElementsAre(1, 2, 3, 4, 5, 6));
+   const auto z = t.zip_map(s, [](auto it, auto is) {
+      return is - it;
+   });
+   ASSERT_THAT(z.storage(), ElementsAre(1, 1, 1, 1, 1, 1));
+}
+
+TEST(test_transform_map_and_set, copy) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(0); // 0, 1, 2, 3, 4, 5
+   const auto s = t.copy();
+   ASSERT_THAT(t.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+   ASSERT_THAT(s.storage(), ElementsAre(0, 1, 2, 3, 4, 5));
+}
+
+TEST(test_transform_map_and_set, set) {
+   auto t = TAT::Tensor<>({"i", "j"}, {2, 3});
+   t.set([i = 0]() mutable {
+      double v[] = {6, 2, 8, 3, 7, 1};
+      return v[i++];
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(6, 2, 8, 3, 7, 1));
+}
+
+TEST(test_transform_map_and_set, set_rvalue) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).set([i = 0]() mutable {
+      double v[] = {6, 2, 8, 3, 7, 1};
+      return v[i++];
+   });
+   ASSERT_THAT(t.storage(), ElementsAre(6, 2, 8, 3, 7, 1));
+}
+
+TEST(test_transform_map_and_set, zero) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).zero();
+   ASSERT_THAT(t.storage(), ElementsAre(0, 0, 0, 0, 0, 0));
+}
+
+TEST(test_transform_map_and_set, range) {
+   const auto t = TAT::Tensor<>({"i", "j"}, {2, 3}).range(3, 2);
+   ASSERT_THAT(t.storage(), ElementsAre(3, 5, 7, 9, 11, 13));
+}
