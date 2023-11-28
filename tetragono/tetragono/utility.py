ab6f8fd027c60b17aa09c249c48a6e9d6c747f8b
Hao Zhang <hzhangxyz@outlook.com>
2023-11-28 12:27:21 +0800

    [tet.py] Add option `use_mpi_io` to toggle whether to use mpi io
    In some platform, MPI IO is slow, so we may need to fallback to gather
    data and write data to file in rank 0 process. The option is controled
    by variable `tetragono.utility.use_mpi_io`.

--- tetragono/tetragono/utility.py
+++ tetragono/tetragono/utility.py
@@ -39,6 +39,8 @@ mpi_comm = MPI.COMM_WORLD
 mpi_rank = mpi_comm.Get_rank()
 mpi_size = mpi_comm.Get_size()
 
+use_mpi_io = True
+
 
 def show(*args, **kwargs):
     if mpi_rank == 0:
@@ -322,31 +324,62 @@ def restrict_wrapper(origin_restrict):
 
 
 def write_configurations(config, file_name):
-    config = np.asarray(config, dtype=np.int64)
-    file = MPI.File.Open(mpi_comm, file_name, MPI.MODE_WRONLY | MPI.MODE_CREATE)
-    shape = config.shape
-    head = np.array([mpi_size, len(shape), *shape], dtype=np.int64)
-    if mpi_rank == 0:
-        file.Write_at(0, head)
-    offset = head.nbytes + mpi_rank * config.nbytes
-    file.Write_at(offset, config)
-    file.Close()
+    if use_mpi_io:
+        config = np.asarray(config, dtype=np.int64)
+        file = MPI.File.Open(mpi_comm, file_name, MPI.MODE_WRONLY | MPI.MODE_CREATE)
+        shape = config.shape
+        head = np.array([mpi_size, len(shape), *shape], dtype=np.int64)
+        if mpi_rank == 0:
+            file.Write_at(0, head)
+        offset = head.nbytes + mpi_rank * config.nbytes
+        file.Write_at(offset, config)
+        file.Close()
+    else:
+        config = np.asarray(config, dtype=np.int64)
+        shape = config.shape
+        all_config = np.zeros([mpi_size, *shape], dtype=np.int64)
+        mpi_comm.Gather(config, all_config, root=0)
+        if mpi_rank == 0:
+            with open(file_name, "wb") as file:
+                head = np.array([mpi_size, len(shape), *shape], dtype=np.int64)
+                file.write(head)
+                file.write(all_config)
 
 
 def read_configurations(file_name):
-    file = MPI.File.Open(mpi_comm, file_name, MPI.MODE_RDONLY)
-    head1 = np.zeros(2, dtype=np.int64)
-    file.Read_at(0, head1)
-    size, config_rank = head1
-    head2 = np.zeros(config_rank, dtype=np.int64)
-    file.Read_at(head1.nbytes, head2)
-    config = np.zeros(head2, dtype=np.int64)
-    if size < mpi_size:
-        with seed_differ:
-            choose = TAT.random.uniform_int(0, size - 1)()
+    if use_mpi_io:
+        file = MPI.File.Open(mpi_comm, file_name, MPI.MODE_RDONLY)
+        head1 = np.zeros(2, dtype=np.int64)
+        file.Read_at(0, head1)
+        size, config_rank = head1
+        head2 = np.zeros(config_rank, dtype=np.int64)
+        file.Read_at(head1.nbytes, head2)
+        config = np.zeros(head2, dtype=np.int64)
+        if size < mpi_size:
+            with seed_differ:
+                choose = TAT.random.uniform_int(0, size - 1)()
+        else:
+            choose = mpi_rank
+        offset = head1.nbytes + head2.nbytes + choose * config.nbytes
+        file.Read_at(offset, config)
+        file.Close()
+        return config
     else:
-        choose = mpi_rank
-    offset = head1.nbytes + head2.nbytes + choose * config.nbytes
-    file.Read_at(offset, config)
-    file.Close()
-    return config
+        with open(file_name, "rb") as file:
+            head1 = np.zeros(2, dtype=np.int64)
+            file.seek(0)
+            file.readinto(head1)
+            size, config_rank = head1
+            head2 = np.zeros(config_rank, dtype=np.int64)
+            file.seek(head1.nbytes)
+            file.readinto(head2)
+            config = np.zeros(head2, dtype=np.int64)
+            if size < mpi_size:
+                with seed_differ:
+                    choose = TAT.random.uniform_int(0, size - 1)()
+            else:
+                choose = mpi_rank
+            offset = head1.nbytes + head2.nbytes + choose * config.nbytes
+            file.seek(offset)
+            file.readinto(config)
+            return config
