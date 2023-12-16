424c53c3dd83adf8df955108fdc022926891bf91
Hao Zhang <hzhangxyz@outlook.com>
2023-12-16 16:23:14 +0800

    [build, TAT.py] Fix dependency of pytat in emscripten wheel.
    This commit try use `pyodide auditwheel` to embed openblas into pytat
    emscripten wheel.
    But this remain in comment since upstream does not support it yet.
    When everything ready just uncomment it.
    And inside `test_wasm.js`, we use micropip.install instead of
    pyodide.loadPackage to handle the dependency correctly.
    The only thing it cannot handle is the openblas, we load it manually
    temporarily.
    
    (close #40)

--- PyTAT/test_wasm.js
+++ PyTAT/test_wasm.js
@@ -2,31 +2,40 @@
 "use strict";
 
 async function main() {
-    const fs = require("fs");
     const process = require("process");
-    const path = require("path");
 
     const pyodide = await require("pyodide").loadPyodide();
-    await pyodide.loadPackage(["scipy", "pytest"]);
-    // clapack/openblas should be loaded manually before loading TAT
-    // but some version of pyodide use clapack but other use openblas
-    // load scipy directly since it always depend on clapack or openblas
-    const dist_dir = "dist";
-    const files = await fs.promises.readdir(dist_dir);
-    await pyodide.loadPackage(path.join(dist_dir, files[0]));
+    await pyodide.loadPackage("micropip");
+    await pyodide.loadPackage("openblas");
+    // openblas should be loaded manually before loading TAT
+    // see https://github.com/ryanking13/auditwheel-emscripten/issues/24
+    // when this been fixed, removing load openblas manually,
+    // and uncomment pyodide auditwheel in github action.
 
-    const mount_dir = "/tests";
+    const mount_dir = "/app";
     pyodide.FS.mkdir(mount_dir);
     pyodide.FS.mount(pyodide.FS.filesystems.NODEFS, {
-        root: "tests"
+        root: "."
     }, mount_dir);
-    const result = await pyodide.runPython(`
-import TAT
-print(TAT())
 
-import pytest
-pytest.main(["${mount_dir}"])
-    `);
+    const result = await pyodide.runPython(`
+async def main():
+    import micropip
+
+    import os
+    files = os.listdir("/app/dist")
+    await micropip.install(f"emfs:/app/dist/{files[0]}")
+
+    import TAT
+    print(TAT())
+
+    await micropip.install("pytest")
+    import pytest
+    return pytest.main(["/app/tests"])
+
+
+main()
+`);
     process.exit(result);
 }
 main();
