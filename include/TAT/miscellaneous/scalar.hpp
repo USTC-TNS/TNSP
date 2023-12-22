3f894c21219ba74e997bae7cb910b09f9e4f5ae6
Hao Zhang <hzhangxyz@outlook.com>
2023-12-22 14:49:48 +0800

    text_scalar

--- include/TAT/miscellaneous/scalar.hpp
+++ include/TAT/miscellaneous/scalar.hpp
@@ -25,25 +25,102 @@
 #include "../utility/timer.hpp"
 
 namespace TAT {
-#define TAT_DEFINE_SCALAR_OPERATOR(OP, EVAL) \
-    template< \
-        typename ScalarType1, \
-        typename ScalarType2, \
-        typename = std::enable_if_t< \
-            (is_complex<ScalarType1> || is_complex<ScalarType2>)&&(!std::is_same_v<real_scalar<ScalarType1>, real_scalar<ScalarType2>>)>> \
-    inline auto OP(const ScalarType1& a, const ScalarType2& b) { \
-        using t = std::common_type_t<ScalarType1, ScalarType2>; \
-        return EVAL; \
-    }
-    TAT_DEFINE_SCALAR_OPERATOR(operator+, t(a) + t(b))
-    TAT_DEFINE_SCALAR_OPERATOR(operator-, t(a) - t(b))
-    TAT_DEFINE_SCALAR_OPERATOR(operator*, t(a) * t(b))
-    TAT_DEFINE_SCALAR_OPERATOR(operator/, t(a) / t(b))
-#undef TAT_DEFINE_SCALAR_OPERATOR
-
     inline timer scalar_outplace_guard("scalar_outplace");
+    inline timer scalar_inplace_guard("scalar_inplace");
 
-#define TAT_DEFINE_SCALAR_OPERATOR(OP, EVAL) \
+    namespace detail {
+        enum arithmetic_type {
+            addition,
+            subtraction,
+            multiplication,
+            division,
+        };
+
+#ifdef TAT_USE_CUDA
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        TAT_CUDA_HOST TAT_CUDA_DEVICE auto arithmetic(const cuda::thrust_complex<ScalarType1>& x, const cuda::thrust_complex<ScalarType2>& y) {
+            using ScalarType = cuda::thrust_complex<std::common_type_t<ScalarType1, ScalarType2>>;
+            if constexpr (type == addition) {
+                return ScalarType(x) + ScalarType(y);
+            } else if constexpr (type == subtraction) {
+                return ScalarType(x) - ScalarType(y);
+            } else if constexpr (type == multiplication) {
+                return ScalarType(x) * ScalarType(y);
+            } else {
+                return ScalarType(x) / ScalarType(y);
+            }
+        }
+
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticTN {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            cuda::thrust_complex<ScalarType2> y;
+            ArithmeticTN(const ScalarType2& number) : y(number) { }
+            TAT_CUDA_HOST TAT_CUDA_DEVICE cuda::thrust_complex<ScalarType> operator()(const cuda::thrust_complex<ScalarType1>& x) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticNT {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            cuda::thrust_complex<ScalarType1> x;
+            ArithmeticNT(const ScalarType1& number) : x(number) { }
+            TAT_CUDA_HOST TAT_CUDA_DEVICE cuda::thrust_complex<ScalarType> operator()(const cuda::thrust_complex<ScalarType2>& y) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticTT {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            TAT_CUDA_HOST TAT_CUDA_DEVICE cuda::thrust_complex<ScalarType>
+            operator()(const cuda::thrust_complex<ScalarType1>& x, const cuda::thrust_complex<ScalarType2>& y) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+#else
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        auto arithmetic(const ScalarType1& x, const ScalarType2& y) {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            if constexpr (type == addition) {
+                return ScalarType(x) + ScalarType(y);
+            } else if constexpr (type == subtraction) {
+                return ScalarType(x) - ScalarType(y);
+            } else if constexpr (type == multiplication) {
+                return ScalarType(x) * ScalarType(y);
+            } else {
+                return ScalarType(x) / ScalarType(y);
+            }
+        }
+
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticTN {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            ScalarType2 y;
+            ArithmeticTN(const ScalarType2& number) : y(number) { }
+            ScalarType operator()(const ScalarType1& x) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticNT {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            ScalarType1 x;
+            ArithmeticNT(const ScalarType1& number) : x(number) { }
+            ScalarType operator()(const ScalarType2& y) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+        template<typename ScalarType1, typename ScalarType2, arithmetic_type type>
+        struct ArithmeticTT {
+            using ScalarType = std::common_type_t<ScalarType1, ScalarType2>;
+            ScalarType operator()(const ScalarType1& x, const ScalarType2& y) {
+                return arithmetic<ScalarType1, ScalarType2, type>(x, y);
+            }
+        };
+#endif
+    } // namespace detail
+
+#define TAT_DEFINE_SCALAR_OPERATOR(OP, type) \
     template< \
         typename ScalarType1, \
         typename ScalarType2, \
@@ -53,7 +130,7 @@ namespace TAT {
     [[nodiscard]] auto OP(const Tensor<ScalarType1, Symmetry, Name>& tensor_1, const Tensor<ScalarType2, Symmetry, Name>& tensor_2) { \
         auto timer_guard = scalar_outplace_guard(); \
         using ScalarType = std::common_type_t<ScalarType1, ScalarType2>; \
-        return tensor_1.template zip_map<ScalarType>(tensor_2, [](const auto& x, const auto& y) { return EVAL; }); \
+        return tensor_1.template zip_map<ScalarType>(tensor_2, detail::ArithmeticTT<ScalarType1, ScalarType2, type>()); \
     } \
     template< \
         typename ScalarType1, \
@@ -64,7 +141,7 @@ namespace TAT {
     [[nodiscard]] auto OP(const Tensor<ScalarType1, Symmetry, Name>& tensor_1, const ScalarType2& number_2) { \
         auto timer_guard = scalar_outplace_guard(); \
         using ScalarType = std::common_type_t<ScalarType1, ScalarType2>; \
-        return tensor_1.template map<ScalarType>([&y = number_2](const auto& x) { return EVAL; }); \
+        return tensor_1.template map<ScalarType>(detail::ArithmeticTN<ScalarType1, ScalarType2, type>(number_2)); \
     } \
     template< \
         typename ScalarType1, \
@@ -75,18 +152,16 @@ namespace TAT {
     [[nodiscard]] auto OP(const ScalarType1& number_1, const Tensor<ScalarType2, Symmetry, Name>& tensor_2) { \
         auto timer_guard = scalar_outplace_guard(); \
         using ScalarType = std::common_type_t<ScalarType1, ScalarType2>; \
-        return tensor_2.template map<ScalarType>([&x = number_1](const auto& y) { return EVAL; }); \
+        return tensor_2.template map<ScalarType>(detail::ArithmeticNT<ScalarType1, ScalarType2, type>(number_1)); \
     }
 
-    TAT_DEFINE_SCALAR_OPERATOR(operator+, x + y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator-, x - y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator*, x* y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator/, x / y)
+    TAT_DEFINE_SCALAR_OPERATOR(operator+, detail::addition)
+    TAT_DEFINE_SCALAR_OPERATOR(operator-, detail::subtraction)
+    TAT_DEFINE_SCALAR_OPERATOR(operator*, detail::multiplication)
+    TAT_DEFINE_SCALAR_OPERATOR(operator/, detail::division)
 #undef TAT_DEFINE_SCALAR_OPERATOR
 
-    inline timer scalar_inplace_guard("scalar_inplace");
-
-#define TAT_DEFINE_SCALAR_OPERATOR(OP, EVAL) \
+#define TAT_DEFINE_SCALAR_OPERATOR(OP, type) \
     template< \
         typename ScalarType1, \
         typename ScalarType2, \
@@ -97,7 +172,7 @@ namespace TAT {
     Tensor<ScalarType1, Symmetry, Name>& OP(Tensor<ScalarType1, Symmetry, Name>& tensor_1, const Tensor<ScalarType2, Symmetry, Name>& tensor_2) { \
         auto timer_guard = scalar_inplace_guard(); \
         tensor_1.acquire_data_ownership("Inplace operator on tensor shared, copy happened here"); \
-        return tensor_1.zip_transform_(tensor_2, [](const auto& x, const auto& y) { return EVAL; }); \
+        return tensor_1.zip_transform_(tensor_2, detail::ArithmeticTT<ScalarType1, ScalarType2, type>()); \
     } \
     template< \
         typename ScalarType1, \
@@ -109,12 +184,12 @@ namespace TAT {
     Tensor<ScalarType1, Symmetry, Name>& OP(Tensor<ScalarType1, Symmetry, Name>& tensor_1, const ScalarType2& number_2) { \
         auto timer_guard = scalar_inplace_guard(); \
         tensor_1.acquire_data_ownership("Inplace operator on tensor shared, copy happened here"); \
-        return tensor_1.transform_([&y = number_2](const auto& x) { return EVAL; }); \
+        return tensor_1.transform_(detail::ArithmeticTN<ScalarType1, ScalarType2, type>(number_2)); \
     }
-    TAT_DEFINE_SCALAR_OPERATOR(operator+=, x + y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator-=, x - y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator*=, x* y)
-    TAT_DEFINE_SCALAR_OPERATOR(operator/=, x / y)
+    TAT_DEFINE_SCALAR_OPERATOR(operator+=, detail::addition)
+    TAT_DEFINE_SCALAR_OPERATOR(operator-=, detail::subtraction)
+    TAT_DEFINE_SCALAR_OPERATOR(operator*=, detail::multiplication)
+    TAT_DEFINE_SCALAR_OPERATOR(operator/=, detail::division)
 #undef TAT_DEFINE_SCALAR_OPERATOR
 } // namespace TAT
 #endif
