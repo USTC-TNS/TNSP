3309c95cff58d8afa9c655aa57428a8404fa7d41
Hao Zhang <hzhangxyz@outlook.com>
2024-07-29 10:39:04 +0800

    [tetku.py] Add complex number for quantum chemistry model.
    Complex model has better connectivity which helps to optimize.

--- tetraku/tetraku/models/openfermion_quantum_chemistry/__init__.py
+++ tetraku/tetraku/models/openfermion_quantum_chemistry/__init__.py
@@ -21,20 +21,8 @@ import TAT
 import tetragono as tet
 from tetragono.common_tensor.tensor_toolkit import rename_io, kronecker_product
 
-Tensor = TAT.FermiZ2.D.Tensor
-EF = ([(False, 1), (True, 1)], False)
-ET = ([(False, 1), (True, 1)], True)
 
-CP = Tensor(["O0", "I0", "T"], [EF, ET, ([(True, 1)], False)]).zero()
-CP[{"O0": (True, 0), "I0": (False, 0), "T": (True, 0)}] = 1
-CM = Tensor(["O0", "I0", "T"], [EF, ET, ([(True, 1)], True)]).zero()
-CM[{"O0": (False, 0), "I0": (True, 0), "T": (True, 0)}] = 1
-I = Tensor(["O0", "I0"], [EF, ET]).identity({("I0", "O0")})
-C0daggerC1 = rename_io(CP, [0]).contract(rename_io(CM, [1]), {("T", "T")})
-C0daggerC1daggerC2C3 = kronecker_product(rename_io(C0daggerC1, [1, 2]), rename_io(C0daggerC1, [0, 3]))
-
-
-def abstract_state(L1, L2, file_name, T=False):
+def abstract_state(L1, L2, file_name, T=False, is_complex=False):
     """
     Create a quantum chemistry state from Hamiltonian in openfermion format.
     Every orbit will be put into a site in lattice L1 * L2.
@@ -48,6 +36,21 @@ def abstract_state(L1, L2, file_name, T=False):
     T : bool
         Whether the total electron number is odd.
     """
+    if is_complex:
+        Tensor = TAT.FermiZ2.C.Tensor
+    else:
+        Tensor = TAT.FermiZ2.D.Tensor
+    EF = ([(False, 1), (True, 1)], False)
+    ET = ([(False, 1), (True, 1)], True)
+
+    CP = Tensor(["O0", "I0", "T"], [EF, ET, ([(True, 1)], False)]).zero()
+    CP[{"O0": (True, 0), "I0": (False, 0), "T": (True, 0)}] = 1
+    CM = Tensor(["O0", "I0", "T"], [EF, ET, ([(True, 1)], True)]).zero()
+    CM[{"O0": (False, 0), "I0": (True, 0), "T": (True, 0)}] = 1
+    I = Tensor(["O0", "I0"], [EF, ET]).identity({("I0", "O0")})
+    C0daggerC1 = rename_io(CP, [0]).contract(rename_io(CM, [1]), {("T", "T")})
+    C0daggerC1daggerC2C3 = kronecker_product(rename_io(C0daggerC1, [1, 2]), rename_io(C0daggerC1, [0, 3]))
+
     state = tet.AbstractState(Tensor, L1, L2)
     state.physics_edges[...] = EF
     state.total_symmetry = T
@@ -73,7 +76,7 @@ def abstract_state(L1, L2, file_name, T=False):
     return state
 
 
-def abstract_lattice(L1, L2, D, file_name, T=False):
+def abstract_lattice(L1, L2, D, file_name, T=False, is_complex=False):
     """
     Create a quantum chemistry lattice from Hamiltonian in openfermion format.
     Every orbit will be put into a site in lattice L1 * L2.
@@ -89,7 +92,7 @@ def abstract_lattice(L1, L2, D, file_name, T=False):
     T : bool
         Whether the total electron number is odd.
     """
-    state = tet.AbstractLattice(abstract_state(L1, L2, file_name, T=T))
+    state = tet.AbstractLattice(abstract_state(L1, L2, file_name, T=T, is_complex=is_complex))
     D1 = D // 2
     D2 = D - D1
     state.virtual_bond["R"] = [(False, D1), (True, D2)]
